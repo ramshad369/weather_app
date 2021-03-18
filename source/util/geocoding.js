807c7261c5f01172062337df3731dcafb9f81656
ramshad <ramshadak369@gmail.com>
2021-03-18 18:15:12 +0530

    some style changes

--- source/util/geocoding.js
+++ source/util/geocoding.js
@@ -4,10 +4,10 @@ var geocode = (address,callback)=>{
     var url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(address)+".json?access_token=pk.eyJ1IjoicmFtc2hhZGFrMzY5IiwiYSI6ImNrbThyZGtzYjFhdTQydnAxcmc3bGl3YnoifQ.uCiR6dppr-cOTzhOfN651w&limit=1"
 request({url, json : true},(err,{body})=>{
     if(err){
-        callback("server error",undefined);
+        callback("Network error",undefined);
     }
     else if(body.features.length==0){
-        callback("client error",undefined);
+        callback("Location not found!",undefined);
     }
     else{
         callback(undefined,{longitude:body.features[0].center[0],
